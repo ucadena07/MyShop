134686646cc41a268c51f328461cbacb356a2b98
DESKTOP-56F9530\ucadena07 <ulises@netkinetix.com>
2020-09-10 06:27:00 -0500

    Added Basket Models

---
+++ MyShop/MyShop.Core/Models/BasketItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class BasketItem : BaseEntity
+    {
+        public string BasketdId { get; set; }
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
